fb5d3ae1beb29dd8240b00dbbae9737b583a7280
Natalia <nkarpova304@gmail.com>
2020-07-28 09:47:15 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package RenataRiver
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+	
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'renatariver' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"> <?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$renatariver_description = get_bloginfo( 'description', 'display' );
+			if ( $renatariver_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $renatariver_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+						<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
---
+++ inc/custom-header.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Sample implementation of the Custom Header feature
+ *
+ * You can add an optional custom header image to header.php like so ...
+ *
+	<?php the_header_image_tag(); ?>
+ *
+ * @link https://developer.wordpress.org/themes/functionality/custom-headers/
+ *
+ * @package RenataRiver
+ */
+
+/**
+ * Set up the WordPress core custom header feature.
+ *
+ * @uses renatariver_header_style()
+ */
+function renatariver_custom_header_setup() {
+	add_theme_support(
+		'custom-header',
+		apply_filters(
+			'renatariver_custom_header_args',
+			array(
+				'default-image'      => '',
+				'default-text-color' => '000000',
+				'width'              => 1000,
+				'height'             => 250,
+				'flex-height'        => true,
+				'wp-head-callback'   => 'renatariver_header_style',
+			)
+		)
+	);
+}
+add_action( 'after_setup_theme', 'renatariver_custom_header_setup' );
+
+if ( ! function_exists( 'renatariver_header_style' ) ) :
+	/**
+	 * Styles the header image and text displayed on the blog.
+	 *
+	 * @see renatariver_custom_header_setup().
+	 */
+	function renatariver_header_style() {
+		$header_text_color = get_header_textcolor();
+
+		/*
+		 * If no custom options for text are set, let's bail.
+		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
+		 */
+		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
+			return;
+		}
+
+		// If we get this far, we have custom styles. Let's do this.
+		?>
+		<style type="text/css">
+		<?php
+		// Has the text been hidden?
+		if ( ! display_header_text() ) :
+			?>
+			.site-title,
+			.site-description {
+				position: absolute;
+				clip: rect(1px, 1px, 1px, 1px);
+				}
+			<?php
+			// If the user has set a custom color for the text use that.
+		else :
+			?>
+			.site-title a,
+			.site-description {
+				color: #<?php echo esc_attr( $header_text_color ); ?>;
+			}
+		<?php endif; ?>
+		</style>
+		<?php
+	}
+endif;
